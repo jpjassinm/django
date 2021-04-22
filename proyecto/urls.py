0a2da2170e0c486b1b986a52dd52cc15cf6c9e02
jpjassinm <jpjassin84@gmail.com>
2021-04-21 22:30:28 -0500

    Se realiza actualizacion de proyecto 2020-04-21

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from proyecto import views # se llama el archivo desde el proyecto y la vista
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
-    path('hello_word/', views.hello_word)
+    path('hello_word/', views.hello_word),
+    path('mensaje_dia/',views.mensaje_dia),
 
 
 
--- proyecto/views.py
+++ proyecto/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
+#DJANGO
+from django.http import HttpResponse # En cada vista se requiere un (resquest) y se importa esta lib
+
+#UTILIDADES
+from datetime import datetime
 
 
-def hello_word(request):
-    return HttpResponse('Hola mundo en Bogota')
+
+#VISTAS
+def hello_word(request): # a esto se le conoce como una vista, hay que recordar que cada vista en una funcion.
+    return HttpResponse('Hola mundo, esto es una vista')
+
+def mensaje_dia(request):
+    #import pdb; pdb.set_trace()
+    now = datetime.now().strftime('%d-%m-%Y  %H:%M:%S')
+    return HttpResponse('Este es el mensaje del dia {now}'.format(now=str(now)))
