fd626010f785a905c7e42b98d59613e74f17b777
jpjassinm <jpjassin84@gmail.com>
2021-04-22 20:21:08 -0500

    actualizacion de mensajes

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from proyecto import views # se llama el archivo desde el proyecto y la vista
+#https://docs.djangoproject.com/en/3.2/topics/http/urls/
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('hello_word/', views.hello_word),
     path('mensaje_dia/',views.mensaje_dia),
+    path('hi/',views.hi),
+    path('menmsaje/',views.mensaje),
 
 
 
--- proyecto/views.py
+++ proyecto/views.py
@@ -14,3 +14,11 @@ def mensaje_dia(request):
     #import pdb; pdb.set_trace()
     now = datetime.now().strftime('%d-%m-%Y  %H:%M:%S')
     return HttpResponse('Este es el mensaje del dia {now}'.format(now=str(now)))
+
+def hi (request):
+    number = [request.GET['numbers=10,20,30'].split(',')]
+    #import pdb; pdb.set_trace() 
+    return HttpResponse(str(numbers), content_type='application/json') #?numbers=10,520,25,2
+
+def mensaje (request):
+    print('Cuidado con lo que haces', content_type='application/json')
