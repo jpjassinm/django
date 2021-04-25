6d864be01841d175df1e518869e21288eeb1cdaf
jpjassinm <jpjassin84@gmail.com>
2021-04-24 21:32:57 -0500

    Ul,timo cambio del modelo de BD

--- posts/models.py
+++ posts/models.py
@@ -3,5 +3,13 @@ from django.db import models
 # Create your models here.
 
 class usuario(models.model):
-    email = models.EmailField()
-    password = models.CharField()
+    
+    id=models.IntegerField(primary_key=True, null=False, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_leng=100)
+    fist_names=models.CharField(max_leng=100)
+    last_name=models.CharField(max_leng=100)
+    bio = models.TextField(max_leng=100)
+    birthdate=models.DateField()
+    created=models.DateField(auto_now_add=True)
+    modified=models.DateTimeField(auto_now=True)
