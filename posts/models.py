cb7c124bdd08cc0956fadc849e4fb98bf3cb4f21
jpjassinm <jpjassin84@gmail.com>
2021-04-30 10:05:44 -0500

    creacion de la tabla

--- posts/models.py
+++ posts/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class persona(models.model):
-    
+class Persona(models.Model):
     
     email = models.EmailField(unique=True)
-    password = models.CharField(max_leng=100)
-    fist_names=models.CharField(max_leng=100)
-    last_name=models.CharField(max_leng=100)
-    bio = models.TextField(max_leng=100)
-    birthdate=models.DateField()
-    created=models.DateField(auto_now_add=True)
-    modified=models.DateTimeField(auto_now=True)
+    password = models.CharField(max_length=100)
+
+    fist_names = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    bio = models.TextField(max_length=100)
+    birthdate = models.DateField(blank=True, null=True)
+
+    created = models.DateField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
